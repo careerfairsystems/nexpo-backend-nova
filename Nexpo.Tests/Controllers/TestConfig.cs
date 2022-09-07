3d7bd410d631e2052c3c8a3071c88ef3d20415a8
Gustav Lindqvist <gustav.lindqvist000@gmail.com>
2022-09-07 21:52:23 +0200

    Started on TestConfig

---
+++ Nexpo.Tests/Controllers/TestConfig.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Nexpo.Controllers;
+using Nexpo.Models;
+using Nexpo.Services;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Nexpo.Tests.Services
+{
+    public class TestConfig
+    {
+        private readonly TokenService _tokenService;
+        private readonly UsersController _usersController;
+
+        public TestConfig()
+        {
+            PasswordService passwordService = new PasswordService();
+            DbContextOptions<ApplicationDbContext> dbContextOptions = new DbContextOptions<ApplicationDbContext>();
+            ApplicationDbContext applicationDbContext = new ApplicationDbContext(dbContextOptions, passwordService);
+            applicationDbContext.Seed();
+
+
+        }
+    }
+}
---
+++ Nexpo.Tests/Controllers/UsersControllerTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Nexpo.Controllers;
+using Nexpo.Services;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Nexpo.Tests.Services
+{
+    public class UsersControllerTest
+    {
+        private readonly TokenService _tokenService;
+        private readonly UsersController _usersController;
+
+        public UsersControllerTest()
+        {
+            var config = new MockConfig
+            {
+                SecretKey = "dGVzdGluZ19zZWNyZXRfa2V5", // base64(testing_secret_key)
+                JWTIssuer = "http://localhost:5000",
+                JWTAudience = "http://localhost:5000",
+                JWTExpires = "72",
+            };
+            _tokenService = new TokenService(config);
+
+            _usersController = new UsersController();
+        }
+
+
+        [Fact]
+        public void testTest()
+        {
+
+        }
+
+        //[Fact]
+        //public void testTest()
+        //{
+        //    var firstTokenString = _tokenService.SignToken(_testData, DateTime.Now.AddHours(1));
+
+        //    Assert.NotEqual(firstTokenString, secondTokenString);
+        //}
+    }
+}
