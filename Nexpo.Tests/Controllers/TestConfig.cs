2cff3d4e8ed2f770444760d40f17216148575948
Gustav Lindqvist <gustav.lindqvist000@gmail.com>
2022-09-08 15:03:39 +0200

    TestConfig not working

--- Nexpo.Tests/Controllers/TestConfig.cs
+++ Nexpo.Tests/Controllers/TestConfig.cs
@@ -2,16 +2,17 @@
 using Microsoft.Extensions.Configuration;
 using Nexpo.Controllers;
 using Nexpo.Models;
+using Nexpo.Repositories;
 using Nexpo.Services;
 using System;
 using System.Collections.Generic;
 using Xunit;
+using Xunit.Sdk;
 
 namespace Nexpo.Tests.Services
 {
     public class TestConfig
     {
-        private readonly TokenService _tokenService;
         private readonly UsersController _usersController;
 
         public TestConfig()
@@ -20,8 +21,20 @@ namespace Nexpo.Tests.Services
             DbContextOptions<ApplicationDbContext> dbContextOptions = new DbContextOptions<ApplicationDbContext>();
             ApplicationDbContext applicationDbContext = new ApplicationDbContext(dbContextOptions, passwordService);
             applicationDbContext.Seed();
+            IUserRepository userRepository = new UserRepository(applicationDbContext);
+            ICompanyConnectionRepository companyConnectionRepository = new CompanyConnectionRepository(applicationDbContext);
+            IStudentSessionApplicationRepository studentSessionApplicationRepository = new StudentSessionApplicationRepository(applicationDbContext);
 
+            _usersController = new UsersController(userRepository, companyConnectionRepository, studentSessionApplicationRepository,passwordService);
 
+        }
+
+        [Fact]
+        public async void TestTest()
+        {
+            //TODO
+            //No database provider has been configured for this DbContext
+            Assert.Equal(null, await _usersController.GetUsers());
         }
     }
 }
