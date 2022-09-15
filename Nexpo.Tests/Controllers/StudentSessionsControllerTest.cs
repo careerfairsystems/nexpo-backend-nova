4e399dabb64e44d38ecb329db70b2d427b41b955
Gustav Lindqvist <gustav.lindqvist000@gmail.com>
2022-09-16 01:06:37 +0200

    Comments ;)

--- Nexpo.Tests/Controllers/StudentSessionsControllerTest.cs
+++ Nexpo.Tests/Controllers/StudentSessionsControllerTest.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json.Linq;
-using Microsoft.OpenApi.Expressions;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json.Nodes;
 using System.Threading.Tasks;
@@ -20,28 +16,31 @@ namespace Nexpo.Tests.Controllers
         [Fact]
         public async Task TestGetStudentSessions()
         {
-            //Test get without auth
+            //Test get without proper auth
             var application = new WebApplicationFactory<Nexpo.Program>();
             var client = application.CreateClient();
             var response = await client.GetAsync("/api/studentsessions");
             Assert.True(response.StatusCode.Equals(HttpStatusCode.Unauthorized), response.StatusCode.ToString());
 
-            //Login
+            //Login with email and password from seeded DB
             var json = new JsonObject();
             json.Add("email", "student1@example.com");
             json.Add("password", "password");
 
-            var content = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
-            response = await client.PostAsync("/api/session/signin", content);
+            var payload = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
+            response = await client.PostAsync("/api/session/signin", payload);
             Assert.True(response.StatusCode.Equals(HttpStatusCode.OK), "Login failed");
+            
+            //Parse token and add "Bearer " for some reason
             string token = new StreamReader(response.Content.ReadAsStream()).ReadToEnd();
             var parser = JObject.Parse(token);
-            token = parser.Value<String>("token");
-            client.DefaultRequestHeaders.Add("Authorization", "Bearer "+ token);
+            token = "Bearer " + parser.Value<String>("token");
+            //Update default header
+            client.DefaultRequestHeaders.Add("Authorization", token);
 
             //Test get with Student auth
             response = await client.GetAsync("/api/studentsessions");
-            Assert.True(response.StatusCode.Equals(HttpStatusCode.OK), token);
+            Assert.True(response.StatusCode.Equals(HttpStatusCode.OK), "Token didn't work: " + token);
         }
     }
 }
