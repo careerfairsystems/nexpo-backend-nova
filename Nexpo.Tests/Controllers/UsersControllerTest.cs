06bce83cfdc6ed37cf4cc12e30089424185b47e9
UsperX <usperx@hotmail.com>
2022-09-15 19:03:21 +0200

    fixed test

--- Nexpo.Tests/Controllers/UsersControllerTest.cs
+++ Nexpo.Tests/Controllers/UsersControllerTest.cs
@@ -1,40 +1,29 @@
-﻿using Nexpo.Controllers;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Nexpo.Controllers;
 using Nexpo.Repositories;
 using Nexpo.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace Nexpo.Tests.Controllers
-{
-    public class UsersControllerTest : IClassFixture<TestDatabaseFixture>
+{ 
+    public class UserControllerTest
     {
-        UsersController _usersController;
-        public UsersControllerTest(TestDatabaseFixture fixture)
-        {
-            Fixture = fixture;
-            var context = Fixture.CreateContext();
-            PasswordService _passwordService = new PasswordService();
-            IUserRepository _userRepository = new UserRepository(context);
-            ICompanyConnectionRepository _companyConnectionRepository = new CompanyConnectionRepository(context);
-            IStudentSessionApplicationRepository _studentSessionApplicationRepository = new StudentSessionApplicationRepository(context);
-            _usersController =
-                new UsersController(_userRepository,
-                _companyConnectionRepository,
-                _studentSessionApplicationRepository,
-                _passwordService);
-        }
-        public TestDatabaseFixture Fixture { get; }
-
 
         [Fact]
-        public void TestGetUsers()
+        public async Task TestGetUsers()
         {
-            var users = _usersController.GetUsers();
-            Assert.Equal("student1", users.ToString());
+            var application = new WebApplicationFactory<Nexpo.Program>();
+            var client = application.CreateClient();
+            var response = await client.GetAsync("/api/users");
+            Assert.True(response.StatusCode.Equals(HttpStatusCode.Unauthorized), response.StatusCode.ToString());
+
         }
     }
 }
