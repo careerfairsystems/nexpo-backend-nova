b4df3c2b6d0b3ef71be16fd1bf9e02b0ccc637fd
Gustav Lindqvist <gustav.lindqvist000@gmail.com>
2022-09-22 18:48:07 +0200

    Testing and updating StudentSessionController

--- Nexpo.Tests/Controllers/StudentSessionsControllerTest.cs
+++ Nexpo.Tests/Controllers/StudentSessionsControllerTest.cs
@@ -8,95 +8,89 @@ using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 using Xunit;
 using System.IO;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Nexpo.Tests.Controllers
 {
     public class StudentSessionsControllerTest
     {
-        [Fact]
-        public async Task TestGetStudentSessions()
+
+        public static async Task<HttpClient> StudentClient()
         {
-            //Test get without proper auth
+            //Create client and login
             var application = new WebApplicationFactory<Nexpo.Program>();
             var client = application.CreateClient();
             var response = await client.GetAsync("/api/studentsessions");
-            Assert.True(response.StatusCode.Equals(HttpStatusCode.Unauthorized), response.StatusCode.ToString());
-
-            //Login with email and password from seeded DB
             var json = new JsonObject();
             json.Add("email", "student1@example.com");
             json.Add("password", "password");
-
             var payload = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
             response = await client.PostAsync("/api/session/signin", payload);
             Assert.True(response.StatusCode.Equals(HttpStatusCode.OK), "Login failed");
-            
-            //Parse token and add "Bearer " for some reason
             string token = new StreamReader(response.Content.ReadAsStream()).ReadToEnd();
             var parser = JObject.Parse(token);
-            token = "Bearer " + parser.Value<String>("token");
-            //Update default header
+            token = "Bearer " + parser.Value<string>("token");
             client.DefaultRequestHeaders.Add("Authorization", token);
+            return client;
+        }
 
-            //Test get with Student auth
-            response = await client.GetAsync("/api/studentsessions");
-            Assert.True(response.StatusCode.Equals(HttpStatusCode.OK), "Token didn't work: " + token);
+        [Fact]
+        public async Task TestGetStudentSessions()
+        {
+            //Test get with student auth
+            var client = await StudentClient();
+            var response = await client.GetAsync("/api/studentsessions");
+            Assert.True(response.StatusCode.Equals(HttpStatusCode.OK), "Auth didn't work");
+            string content = new StreamReader(response.Content.ReadAsStream()).ReadToEnd();
+            content = content.Replace("[", " ").Replace("]", " ").Trim();
+            var parsedContent = JObject.Parse(content);
+            var stringContent = parsedContent.Value<string>("id");
+            Assert.Equal("1", stringContent);
         }
 
         [Fact]
         public async Task TestGetSingleStudentSessions()
         {
-            //Test get without proper auth
-            var application = new WebApplicationFactory<Nexpo.Program>();
-            var client = application.CreateClient();
+            var client = await StudentClient();
             var response = await client.GetAsync("/api/studentsessions/1");
-            Assert.True(response.StatusCode.Equals(HttpStatusCode.Unauthorized), response.StatusCode.ToString());
-
-            //Login with email and password from seeded DB
-            var json = new JsonObject();
-            json.Add("email", "student1@example.com");
-            json.Add("password", "password");
-
-            var payload = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
-            response = await client.PostAsync("/api/session/signin", payload);
-            Assert.True(response.StatusCode.Equals(HttpStatusCode.OK), "Login failed");
-
-            //Parse token and add "Bearer " for some reason
-            string token = new StreamReader(response.Content.ReadAsStream()).ReadToEnd();
-            var parser = JObject.Parse(token);
-            token = "Bearer " + parser.Value<String>("token");
-            //Update default header
-            client.DefaultRequestHeaders.Add("Authorization", token);
-
-            //Test get with Student auth
-            response = await client.GetAsync("/api/studentsessions/1");
-            Assert.True(response.StatusCode.Equals(HttpStatusCode.OK), response.StatusCode + "Token didn't work: " + token);
+            Assert.True(response.StatusCode.Equals(HttpStatusCode.OK), response.StatusCode + "Token didn't work");
+            string content = new StreamReader(response.Content.ReadAsStream()).ReadToEnd();
+            var parsedContent = JObject.Parse(content);
+            var stringContent = parsedContent.Value<string>("status");
+            Assert.Equal("0", stringContent);
         }
 
-        [Fact]
-        public async Task TestPutSingleStudentSessions()
-        {
-            var application = new WebApplicationFactory<Nexpo.Program>();
-            var client = application.CreateClient();
+        //[Fact]
+        //public async Task TestPutSingleStudentSessions()
+        //{
+        //    //Test original value
+        //    var client = await StudentClient();
+        //    var response = await client.GetAsync("/api/studentsessions/1");
+        //    string content = new StreamReader(response.Content.ReadAsStream()).ReadToEnd();
+        //    var parsedContent = JObject.Parse(content);
+        //    var stringContent = parsedContent.Value<string>("status");
+        //    Assert.True(stringContent.Equals("1"), parsedContent.ToString());
+            
+        //    //Send DTO??
+        //    var json = new JsonObject();
+        //    json.Add("status", "1");
+        //    var payload = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
+        //    response = await client.PutAsync("/api/studentsessions/1", payload);
+        //    Assert.True(HttpStatusCode.OK.Equals(response.StatusCode), json.ToString());
 
-            //Login with email and password from seeded DB
-            var json = new JsonObject();
-            json.Add("email", "student1@example.com");
-            json.Add("password", "password");
-            var payload = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
-            var response = await client.PostAsync("/api/session/signin", payload);
-            Assert.True(response.StatusCode.Equals(HttpStatusCode.OK), "Login failed");
-            string token = new StreamReader(response.Content.ReadAsStream()).ReadToEnd();
-            var parser = JObject.Parse(token);
-            token = "Bearer " + parser.Value<String>("token");
-            client.DefaultRequestHeaders.Add("Authorization", token);
 
-            //Test put with Student auth
-            json = new JsonObject();
-            json.Add("Id", "1");
-            payload = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
-            response = await client.PutAsync("/api/studentsessions/1", payload);
-            Assert.True(response.StatusCode.Equals(HttpStatusCode.OK), response.StatusCode.ToString());
-        }
+        //    response = await client.GetAsync("/api/studentsessions/1");
+        //    content = new StreamReader(response.Content.ReadAsStream()).ReadToEnd();
+        //    parsedContent = JObject.Parse(content);
+        //    stringContent = parsedContent.Value<string>("status");
+        //    Assert.Equal("1", stringContent);
+
+
+        //    json = new JsonObject();
+        //    json.Add("status", "0");
+        //    payload = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
+        //    response = await client.PutAsync("/api/studentsessions/1", payload);
+        //    Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        //}
     }
 }
