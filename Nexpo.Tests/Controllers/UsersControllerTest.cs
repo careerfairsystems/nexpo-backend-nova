2e01a934dbc36181480136f545b46256f4109f21
UsperX <usperx@hotmail.com>
2022-10-04 09:41:18 +0200

    Update getUsers test

--- Nexpo.Tests/Controllers/UsersControllerTest.cs
+++ Nexpo.Tests/Controllers/UsersControllerTest.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Nexpo.Controllers;
+using Nexpo.Models;
 using Nexpo.Repositories;
 using Nexpo.Services;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -16,13 +21,47 @@ namespace Nexpo.Tests.Controllers
     public class UserControllerTest
     {
 
+
+        private async Task<String> Login(string role, HttpClient client)
+        {
+            var json = new JsonObject();
+            switch (role)
+            {
+                case "company":
+                    json.Add("email", "rep1@company1.example.com");
+                    json.Add("password", "password");
+                    break;
+                case "admin":
+                    json.Add("email", "admin@example.com");
+                    json.Add("password", "password");
+                    break;
+                default:
+                    json.Add("email", "student1@example.com");
+                    json.Add("password", "password");
+                    break;
+            }
+
+            var payload = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("/api/session/signin", payload);
+            string token = new StreamReader(response.Content.ReadAsStream()).ReadToEnd();
+            var parser = JObject.Parse(token);
+            token = "Bearer " + parser.Value<String>("token");
+            client.DefaultRequestHeaders.Add("Authorization", token);
+            return token;
+        }
+
         [Fact]
         public async Task TestGetUsers()
         {
             var application = new WebApplicationFactory<Nexpo.Program>();
             var client = application.CreateClient();
-            var response = await client.GetAsync("/api/users");
-            Assert.True(response.StatusCode.Equals(HttpStatusCode.Unauthorized), response.StatusCode.ToString());
+            var token = await Login("admin", client);
+            var response = await client.GetAsync("/api/users/");
+
+            string responseText = await response.Content.ReadAsStringAsync();
+            var responseList = JsonConvert.DeserializeObject<List<User>>(responseText);
+         
+            Assert.True(responseText == "Test", responseText.ToString());
 
         }
     }
