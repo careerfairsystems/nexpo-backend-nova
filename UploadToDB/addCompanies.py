f80992a6852ece675bc451ee2cb4b8a2d1f3e4a1
Matfolkarn <Hampus.mattfolk@gmail.com>
2022-10-13 23:17:25 +0200

    AddCompanies. Script and CompaniesController

--- UploadToDB/addCompanies.py
+++ UploadToDB/addCompanies.py
@@ -3,74 +3,80 @@ from re import I
 from typing import Dict
 import pandas as pd
 
+import requests
 
-with open('ArkadIndent2.json', encoding="utf-8") as d:
+jsonfile = 'ArkadIndent2.json'
+url = 'http://localhost/api/companies/add'
+s3BucketUrl = 'https://nexpo-bucket.s3.eu-north-1.amazonaws.com/'
+
+with open(jsonfile, encoding="utf-8") as d:
     dictData = json.load(d)
-
-
-
 
 df = pd.DataFrame(dictData)
 
-
 for row in range(len(df)):
-    
-   
     pr = df.iloc[row]
     prof = pr['profile']
- 
-    
     if type(prof) == dict:
     
         if 'name' in prof:
-            name = prof['name']
-        else: name = ""
+            name: str = '"' + prof['name'] + '"'
+        else: name = '"' +"" + '"' +'"' 
 
         if 'weOffer' in prof:
-            weOffer = prof['weOffer']
+            weOffer: str = prof['weOffer'] 
         else:
-            weOffer = ""
+            weOffer = '"' + "" + '"' 
 
         if 'desiredDegree' in prof:
-            desiredDegree = prof['desiredDegree']
+            desiredDegree: str = prof['desiredDegree']
         else:
             desiredDegree = ""
 
         if 'industry' in prof:
-            industry = prof['industry']
+            industry: str = prof['industry']
         else:
             industry = ""
 
         if 'desiredProgramme' in prof:
-            desiredProgramme = prof['desiredProgramme']
+            desiredProgramme: str = prof['desiredProgramme']
         else:
             desiredProgramme  = ""
 
         if 'didYouKnow' in prof:
-            didYouKnow = prof['didYouKnow']
+            did: str = prof['didYouKnow'].replace('"','\"') .replace('\n', '').replace(chr(0x0B), '').replace(chr(0x09),'')
+            didYouKnow: str = '"' + did + '"'
         else:
-            didYouKnow = ""
+            didYouKnow = '"' +"" + '"' 
 
         if 'aboutUs' in prof:
-            aboutUs = prof['aboutUs']
+            desc = prof['aboutUs'].replace('"','\"').replace('\n', '').replace(chr(0x0B), '').replace(chr(0x09),'')
+            description: str = '"' + desc + '"'
         else:
-            aboutUs = ""
+            description = '"' +"" + '"' 
 
         if 'urlWebsite' in prof:
-            urlWebsite = prof['urlWebsite']
+            website:str = '"' + prof['urlWebsite'] + '"'
         else:
-            urlWebsite = ""
+            website = '"' +"" + '"' 
 
         if 'logotype' in prof and prof['logotype'] != None:
 
-            pictureFileName = prof['logotype']['name']
-            pictureFileName = pictureFileName.replace('eps','jpg')
+            logoUrl  = prof['logotype']['name']
+            logoUrl: str  = '"' + s3BucketUrl + logoUrl.replace('eps','jpg') + '"'
 
         else:
-            pictureFileName = ""
+            logoUrl  =  '"' +""+ '"' 
 
-        print(weOffer,"\n",  name,"\n",  desiredDegree,"\n",  industry,"\n",  desiredProgramme,"\n",  didYouKnow,"\n",  aboutUs,"\n",  urlWebsite, "\n", pictureFileName)
   
+        headers = {
+            'accept': 'text/plain',
+            'Content-Type': 'application/json'
+        }
+
         
-
-
+        data = '{ "name":' + name + ', "description":' + description +', "didYouKnow":' + didYouKnow + ', "website":' + website + ', "logoUrl":' + logoUrl + ' }'
+        
+        r = requests.put(url, data=data.encode('utf-8'), headers=headers)
+        print(r)
+        print(r.content)
