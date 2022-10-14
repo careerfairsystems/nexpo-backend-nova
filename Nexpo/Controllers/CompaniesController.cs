edff0e235222e1815f9a3945ac33b505ca93402a
Matfolkarn <Hampus.mattfolk@gmail.com>
2022-10-14 18:37:59 +0200

    Still in work, trying to add Enum: Degrees

--- Nexpo/Controllers/CompaniesController.cs
+++ Nexpo/Controllers/CompaniesController.cs
@@ -169,12 +169,14 @@ namespace Nexpo.Controllers
         /// </summary>
         [HttpPut]
         [Route("add")]
+        //IN WORK
         //[Authorize(Roles = nameof(Role.Administrator))]
         [ProducesResponseType(typeof(Company), StatusCodes.Status200OK)]
         public async Task<ActionResult> AddNewCompany(AddCompanyDto dto)
         {
-            var company = new Company{
-                Name =  dto.Name,
+            
+            var company = new Company {
+                Name = dto.Name,
                 Description = dto.Description,
                 DidYouKnow = dto.DidYouKnow,
                 LogoUrl = dto.LogoUrl,
@@ -183,6 +185,26 @@ namespace Nexpo.Controllers
             await _companyRepo.Add(company);
 
             return Ok("company");
+        }
+
+        [HttpPut]
+        [Route("adDegree")]
+        //[Authorize(Roles = nameof(Role.Administrator))]
+        //IN WORK
+        [ProducesResponseType(typeof(Company), StatusCodes.Status200OK)]
+        public async Task<ActionResult> AddNewDegree(AddDegreeDto dto, int companyId)
+        {
+
+            var company = await _companyRepo.Get(companyId);
+            if (company.DesiredDegrees == null)
+            {
+                company.DesiredDegrees = new List<Degree>();
+            }
+            company.DesiredDegrees.Add(dto.degree);
+
+            await _companyRepo.Update(company);
+
+            return Ok(company);
         }
     }
 }
