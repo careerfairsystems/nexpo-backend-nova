2d20ec4e916aa9ba1e9fec93cf9454ffac3cc2ca
@nils.alexander.hansson <nils.alexander.hansson@gmail.com>
2023-03-19 16:11:29 +0100

    updated bash file

--- runBackend.sh
+++ runBackend.sh
@@ -3,3 +3,5 @@ if(docker ps -a | grep nexpo_database); then
 fi
 sudo docker run -d --name nexpo_database -p 5432:5432 -e POSTGRES_USER=nexpo -e POSTGRES_PASSWORD=nexpo postgres:14
 dotnet run --project Nexpo
+# Första gången: Kör denna fil
+# Sedan: Kör endast dotnet run --project Nexpo
