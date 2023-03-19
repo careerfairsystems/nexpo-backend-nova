fe8663d1b2beeed7fca6b3d1317cad5748b4c12d
@nils.alexander.hansson <nils.alexander.hansson@gmail.com>
2023-03-19 04:33:52 +0100

    created sh file to run

---
+++ runBackend.sh
@@ -0,0 +1,5 @@
+if(docker ps -a | grep nexpo_database); then
+    docker rm -f nexpo_database
+fi
+docker run -d --name nexpo_database -p 5432:5432 -e POSTGRES_USER=nexpo -e POSTGRES_PASSWORD=nexpo postgres:14
+dotnet run --project Nexpo
