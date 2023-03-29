79675adbde4addedebd1859d04bc3ba41719dfd3
@nils.alexander.hansson <nils.alexander.hansson@gmail.com>
2023-03-29 16:41:18 +0200

    Kommentar

--- runBackend.sh
+++ runBackend.sh
@@ -38,12 +38,8 @@ while getopts ":h:q:s" opt; do
         git config --local alias.run '!sh ./runBackend.sh'
         exit 1
         ;;
-    s|standalone)
-        # Make a standalone docker run. Does not use docker-compose.
-        # Less relaiable, but an alternative
-        sudo docker rm -f nexpo_database
-        sudo docker run -d --name nexpo_database -p 5432:5432 -e POSTGRES_USER=nexpo -e POSTGRES_PASSWORD=nexpo postgres:14
-        dotnet run --project Nexpo
+    c|compose)
+        sudo docker-compose up -d
         exit 1
         ;;
     esac
@@ -52,5 +48,7 @@ done
 checkInstalled docker
 checkInstalled dotnet
 
-sudo docker-compose up -d
+sudo docker rm -f nexpo_database
+sudo docker run -d --name nexpo_database -p 5432:5432 -e POSTGRES_USER=nexpo -e POSTGRES_PASSWORD=nexpo postgres:14
+dotnet run --project Nexpo
 
