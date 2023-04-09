641dfd134932e46c82555f21664bc26ca5422d61
@nils.alexander.hansson <nils.alexander.hansson@gmail.com>
2023-04-10 00:01:14 +0200

    Refactoring

--- Nexpo/Controllers/SamlController.cs
+++ Nexpo/Controllers/SamlController.cs
@@ -32,6 +32,11 @@ namespace Nexpo.Controllers
 
         }
 
+        /// <summary>
+        /// SSO SingleSignOnService endpoint
+        ///
+        /// The endpoint that sends a SAML authentication request to the IdP
+        /// </summary>
         [EnableCors]
         [AllowAnonymous]
         [HttpGet("InitiateSingleSignOn")]
@@ -49,9 +54,94 @@ namespace Nexpo.Controllers
         }
 
         /// <summary>
-        /// the endpoint corresponding to the entityID in the metadata
-        /// Used it to display metadata
-        /// also want to use to automatically generate metadata
+        /// SSO AssertionConsumerService endpoint
+        ///
+        /// The endpoint that the IdP will post the SAMLResponse to
+        /// ACS is an abbreviation for AttributeConsumerService
+        /// </summary>
+        [AllowAnonymous]
+        [HttpPost("ACS")]
+        public async Task<IActionResult> AttributeConsumerService()
+        {
+            var authenticateResult = await HttpContext.AuthenticateAsync(ApplicationSamlConstants.External);
+
+            if (!authenticateResult.Succeeded)
+            {
+                return Unauthorized();
+            }
+
+            var token = this.CreateJwtSecurityToken(authenticateResult);
+            HttpContext.Session.SetString("JWT", new JwtSecurityTokenHandler().WriteToken(token));
+
+            return this.Ok();
+        }
+
+        /// <summary>
+        /// SSO SingleLogoutService (SLO) endpoint
+        ///
+        /// The endpoint the Identity Provider (IdP) sends logout requests to,
+        /// in order to terminate the user's session.
+        /// </summary>
+        [AllowAnonymous]
+        [HttpGet("Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(Saml2Defaults.Scheme);
+
+            return this.Ok();
+        }
+
+        [AllowAnonymous]
+        [HttpGet("Callback")]
+        public async Task<IActionResult> LoginCallback(string returnUrl)
+        {
+            var authenticateResult = await HttpContext.AuthenticateAsync(ApplicationSamlConstants.External);
+
+            if (!authenticateResult.Succeeded)
+            {
+                return Unauthorized();
+            }
+
+            var token = this.CreateJwtSecurityToken(authenticateResult);
+            HttpContext.Session.SetString("JWT", new JwtSecurityTokenHandler().WriteToken(token));
+
+            if (!string.IsNullOrEmpty(returnUrl))
+            {
+                return Redirect(returnUrl);
+
+            }
+
+            return this.Ok();
+        }
+
+        private JwtSecurityToken CreateJwtSecurityToken(AuthenticateResult authenticateResult)
+        {
+            var claimsIdentity = new ClaimsIdentity(ApplicationSamlConstants.Application);
+            claimsIdentity.AddClaim(authenticateResult.Principal.FindFirst(ClaimTypes.NameIdentifier));
+
+            var username = authenticateResult.Principal.FindFirst(ClaimTypes.NameIdentifier).Value.ToString();
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Sub, username),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.configuration["JWT:Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            return new JwtSecurityToken(
+                this.configuration["JWT:Issuer"],
+                this.configuration["JWT:Issuer"],
+                claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: credentials);
+        }
+
+        /// <summary>
+        /// The endpoint corresponding to the entityID in the metadata
+        /// 
+        /// Plans to be used to generate metadata
         /// </summary>
         [HttpGet("SP")]
         public Task<IActionResult> SP()
@@ -139,81 +229,6 @@ namespace Nexpo.Controllers
             */
 
             
-        }
-
-
-        [AllowAnonymous]
-        [HttpGet("Callback")]
-        public async Task<IActionResult> LoginCallback(string returnUrl)
-        {
-            var authenticateResult = await HttpContext.AuthenticateAsync(ApplicationSamlConstants.External);
-
-            if (!authenticateResult.Succeeded)
-            {
-                return Unauthorized();
-            }
-
-            var token = this.CreateJwtSecurityToken(authenticateResult);
-            HttpContext.Session.SetString("JWT", new JwtSecurityTokenHandler().WriteToken(token));
-
-            if (!string.IsNullOrEmpty(returnUrl))
-            {
-                return Redirect(returnUrl);
-
-            }
-
-            return this.Ok();
-        }
-
-        //ACS is an abbreviation for AttributeConsumerService
-        [AllowAnonymous]
-        [HttpPost("ACS")]
-        public async Task<IActionResult> AttributeConsumerService()
-        {
-            var authenticateResult = await HttpContext.AuthenticateAsync(ApplicationSamlConstants.External);
-
-            if (!authenticateResult.Succeeded)
-            {
-                return Unauthorized();
-            }
-
-            var token = this.CreateJwtSecurityToken(authenticateResult);
-            HttpContext.Session.SetString("JWT", new JwtSecurityTokenHandler().WriteToken(token));
-
-            return this.Ok();
-        }
-
-        [AllowAnonymous]
-        [HttpGet("Logout")]
-        public async Task<IActionResult> Logout()
-        {
-            await HttpContext.SignOutAsync(Saml2Defaults.Scheme);
-
-            return this.Ok();
-        }
-
-        private JwtSecurityToken CreateJwtSecurityToken(AuthenticateResult authenticateResult)
-        {
-            var claimsIdentity = new ClaimsIdentity(ApplicationSamlConstants.Application);
-            claimsIdentity.AddClaim(authenticateResult.Principal.FindFirst(ClaimTypes.NameIdentifier));
-
-            var username = authenticateResult.Principal.FindFirst(ClaimTypes.NameIdentifier).Value.ToString();
-
-            var claims = new List<Claim>
-            {
-                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim(JwtRegisteredClaimNames.Sub, username),
-            };
-
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.configuration["JWT:Key"]));
-            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-            return new JwtSecurityToken(
-                this.configuration["JWT:Issuer"],
-                this.configuration["JWT:Issuer"],
-                claims,
-                expires: DateTime.Now.AddHours(1),
-                signingCredentials: credentials);
         }
 
 
