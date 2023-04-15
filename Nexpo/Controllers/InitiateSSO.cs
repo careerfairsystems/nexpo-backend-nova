646cd9b9af0387977961203f7553eabf5147d2d3
@nils.alexander.hansson <nils.alexander.hansson@gmail.com>
2023-04-16 01:30:29 +0200

    now using scheme

--- Nexpo/Controllers/InitiateSSO.cs
+++ Nexpo/Controllers/InitiateSSO.cs
@@ -23,6 +23,7 @@ namespace Nexpo.Controllers
         }
 
 
+        
         /// <summary>
         /// The Single Sign-On (SSO) Service endpoint
         ///
@@ -33,53 +34,17 @@ namespace Nexpo.Controllers
         [HttpGet("InitiateSingleSignOn")]
         public IActionResult InitiateSingleSignOn()
         {
+            // Create authentication properties to store the relay state, which is used to redirect the user back to the correct URL
+            var authenticationProperties = new AuthenticationProperties
+            {
+                RedirectUri = "/", 
+                // Redirect to the home page after the SSO process is complete
+                // better to redirect them to a logged in page? are they authorized?
+            };
 
-            // Get the desired identity provider (IdP) endpoint URL
-            string idpSSOServiceEndpoint = "https://idpv4.lu.se/idp/profile/Shibboleth/SSO";
-
-            // Create a unique identifier and a timestamp for the SAMLRequest
-            string id = "_" + Guid.NewGuid().ToString();
-            DateTime now = DateTime.UtcNow;
-            string issueInstant = now.ToString("yyyy-MM-ddTHH:mm:ssZ");
-
-            // Create the SAMLRequest XML document
-            XmlDocument doc = new XmlDocument();
-            XmlElement root = doc.CreateElement("samlp", "AuthnRequest", "urn:oasis:names:tc:SAML:2.0:protocol");
-            root.SetAttribute("ID", id);
-            root.SetAttribute("Version", "2.0");
-            root.SetAttribute("IssueInstant", issueInstant);
-            root.SetAttribute("ProtocolBinding", "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect");
-            root.SetAttribute("AssertionConsumerServiceURL", "https://www.nexpo.arkadtlth.se/api/Saml/ACS");
-            root.SetAttribute("Destination", idpSSOServiceEndpoint);
-            doc.AppendChild(root);
-
-            XmlElement issuer = doc.CreateElement("saml", "Issuer", "urn:oasis:names:tc:SAML:2.0:assertion");
-            issuer.InnerText = "https://www.nexpo.arkadtlth.se/api/saml/SP";
-            root.AppendChild(issuer);
-
-            XmlElement nameidPolicy = doc.CreateElement("samlp", "NameIDPolicy", "urn:oasis:names:tc:SAML:2.0:protocol");
-            nameidPolicy.SetAttribute("Format", "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified");
-            nameidPolicy.SetAttribute("AllowCreate", "true");
-            root.AppendChild(nameidPolicy);
-
-            // Sign the SAMLRequest using a private key
-            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
-            string privateKeyXml = "<RSAKeyValue><Modulus>...</Modulus><Exponent>...</Exponent><D>...</D><P>...</P><Q>...</Q><DP>...</DP><DQ>...</DQ><InverseQ>...</InverseQ></RSAKeyValue>";
-            rsa.FromXmlString(privateKeyXml);
-            SignedXml signedXml = new SignedXml(doc);
-            signedXml.SigningKey = rsa;
-            Reference reference = new Reference("#" + id);
-            reference.AddTransform(new XmlDsigEnvelopedSignatureTransform());
-            reference.AddTransform(new XmlDsigExcC14NTransform());
-            signedXml.AddReference(reference);
-            signedXml.ComputeSignature();
-            XmlElement signature = signedXml.GetXml();
-            root.AppendChild(signature);
-
-            // Convert the SAMLRequest to a URL-encoded string and redirect the user
-            string samlRequest = Convert.ToBase64String(Encoding.UTF8.GetBytes(doc.OuterXml));
-            string redirectUrl = idpSSOServiceEndpoint + "?SAMLRequest=" + HttpUtility.UrlEncode(samlRequest);
-            return Redirect(redirectUrl);
+            // Initiate the SAML authentication process for the user
+            return Challenge(authenticationProperties, "Saml2");
+            
             
             
                 
