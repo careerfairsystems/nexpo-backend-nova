47c917547c204bd2a935af52226b6bf1f0b80b24
@nils.alexander.hansson <nils.alexander.hansson@gmail.com>
2023-04-16 03:03:14 +0200

    påbörjade en saml2Handler (krav för att fungera)

---
+++ Nexpo/Helpers/SamlHandler.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Sustainsys.Saml2.AspNetCore2;
+
+namespace Nexpo.Helpers
+{
+    public class SamlHandler : Saml2Handler
+    {
+        private readonly IOptions<Saml2Options> _options;
+        private readonly ILogger _logger;
+
+        public SamlHandler(IOptionsMonitorCache<Saml2Options> optionsCache, IDataProtectionProvider dataProtectorProvider, IOptionsFactory<Saml2Options> optionsFactory) 
+        : base(optionsCache, dataProtectorProvider, optionsFactory)
+        {
+            
+        }
+
+        public AuthenticateResult HandleAuthenticateAsync(AuthenticateContext context){
+            throw new NotImplementedException();
+        }
+
+        public Task HandleSignOutAsync(SignOutContext context){
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+
+}
