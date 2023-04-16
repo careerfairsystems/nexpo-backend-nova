e5fc9edab340cf2d4636f1010b1c1bd8058762fd
@nils.alexander.hansson <nils.alexander.hansson@gmail.com>
2023-04-16 03:04:00 +0200

    cleanup

--- Nexpo/Controllers/SamlController.cs
+++ Nexpo/Controllers/SamlController.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Http;
 using Nexpo.Constants;
+using Microsoft.AspNetCore.Cors;
 
 namespace Nexpo.Controllers
 {
@@ -99,84 +100,7 @@ namespace Nexpo.Controllers
             var xmlContent = System.IO.File.ReadAllText(metadataPath);
             return Task.FromResult<IActionResult>(Content(xmlContent, "text/xml"));
 
-            /*
-
-            var saml2Options = new Saml2Options();
-            configuration.GetSection("Saml2").Bind(saml2Options); //rätt section?
-            var SPOptions = saml2Options.SPOptions;
-            //var serviceProvider = (ServiceProvider) Nexpo.Startup.ServiceProvider;
-
-
-            //config entityID
-            var entityDescriptor = new EntityDescriptor(SPOptions.EntityId);
-
-
-            var SPX509cert = SPOptions.ServiceCertificates[0];
-            var SPX509Data = new KeyInfoX509Data();
-            SPX509Data.AddCertificate(SPX509cert.Certificate);
-            var keyInfo = new DSigKeyInfo();
-            //maybe add SPX509Data to keyInfo
-
-
-            var SPX509Descriptor = new KeyDescriptor
-            {
-                Use = KeyType.Signing,
-                KeyInfo = keyInfo,
-            };
-
-
-            //entityDescriptor.keyDiscriptors.Add(SPX509Descriptor);
-
-            //config endpoints
-            var spd = new SpSsoDescriptor();
-            var ACS = new AssertionConsumerService
-            {
-                Binding = new Uri("urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"),
-                Location = new Uri("https://www.nexpo.arkadtlth.se/api/saml/ACS"), //fullt att hårdkoda - refactor
-                Index = 0,
-            };
-
-
-            var SLO = new SingleLogoutService
-            {
-                Binding = new Uri("urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"),
-                Location = new Uri("https://www.nexpo.arkadtlth.se/api/saml/Logout"),
-            };
-
-
-            spd.AssertionConsumerServices.TryAdd(0, ACS);
-            spd.SingleLogoutServices.Add(SLO);
-            spd.WantAssertionsSigned = SPOptions.WantAssertionsSigned;
-            entityDescriptor.RoleDescriptors.Add(spd);
-
-            var callback = new Uri("https://www.nexpo.arkadtlth.se/api/saml/Callback");
-
-            //var technicalContactMail = "it.arkad@tlth.se"; //lägg i config/constants   
-            //var tokenHandler = SPOptions.Saml2PSecurityTokenHandler;
-            //
-            //var nameIdFormat = new Uri("urn:oasis:names:tc:SAML:2.0:nameid-format:persistent"); //rätt?
-
-            var xmlDoc = new XmlDocument();
-            var xmlDeclaration = xmlDoc.CreateXmlDeclaration("1.0", "UTF-8", null);
-            xmlDoc.AppendChild(xmlDeclaration);
-
-            var entityDescriptorXMLChild = xmlDoc.CreateElement(entityDescriptor.Extensions.ToString());
-            xmlDoc.AppendChild(entityDescriptorXMLChild);
-            //NameId Format - kolla om rätt n
-            //private key
-            //WantAssertionsSigned
-            //callback - lägg till
-            //baseURL
-
-
-            //kolla på add ta all info från SPOptions och lägg till i xml
-
-
-            //outer?
-            //return Content(xmlDoc.InnerText, "text/xml");
-
-            */
-
+            
             
         }
 
