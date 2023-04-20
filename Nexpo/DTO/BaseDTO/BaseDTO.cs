7d23829d67754c46091301e192caf5494d10612b
@nils.alexander.hansson <nils.alexander.hansson@gmail.com>
2023-04-20 18:02:33 +0200

    Started making a baseDTO but not in use

--- Nexpo/DTO/BaseDTO/BaseDTO.cs
+++ Nexpo/DTO/BaseDTO/BaseDTO.cs
@@ -1,11 +1,21 @@
 namespace Nexpo.DTO
 {
-    public abstract class BaseDTO<DTOType, EntryType>
+    public abstract class BaseDTO<EntryType>
     {
+        // CURRENTLY UNUSED
 
-        // Apply the DTOType to all entries
+        /// <summary>
+        /// Applies the values of this DTO to the given EntryType
+        /// </summary>
+        /// <param name="entries">The EntryType to apply the values to.
+        ///                       Refers to Model entries, such as "Contact"
+        ///                       That match the DTO</param>
+        /// <remarks> Note that the method only works if the DTO and the EntryType
+        ///           have the same property names. </remarks>
         public virtual EntryType ApplyToEntries(EntryType entries)
         {
+            //Lös null check. Gör så det bara fungerar på primitiva typer. 
+            // Lös med required attribut.
             var entryAttributes = entries.GetType().GetProperties();
             var dtoAttributes = this.GetType().GetProperties();
 
