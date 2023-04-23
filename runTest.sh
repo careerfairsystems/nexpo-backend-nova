aeb90af999e33bd55b2754947d886e5f280cfc2b
Alexander Hansson <90635063+AlexanderHansson4225@users.noreply.github.com>
2023-04-23 21:21:32 +0200

    Deleted duplicate .sh files

--- runTest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" = "-reset" ]; then
-    # Reset the database before running the tests
-    sudo docker rm -f nexpo_database
-    sudo docker run -d --name nexpo_database -p 5432:5432 -e POSTGRES_USER=nexpo -e POSTGRES_PASSWORD=nexpo postgres:14
-fi
-
-sudo dotnet test Nexpo.Tests/
