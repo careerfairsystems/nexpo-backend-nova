e3368aa8e62452e18b9f3c0f87dda81684eb0b58
Alexander Hansson <nils.alexander.hansson@gmail.com>
2023-04-27 21:55:58 +0200

    Merged days at arkad and migrated

---
+++ Nexpo/Migrations/20230427195514_DaysAtArkad.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nexpo.Migrations
+{
+    public partial class DaysAtArkad : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<int>>(
+                name: "DaysAtArkad",
+                table: "Companies",
+                type: "integer[]",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DaysAtArkad",
+                table: "Companies");
+        }
+    }
+}
