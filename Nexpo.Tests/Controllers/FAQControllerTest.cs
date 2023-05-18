55558c6ab894faffcff5855cdbd5adcc0707da43
Sandra Christoffersen <sandrachristoffersen00@gmail.com>
2023-05-18 19:07:33 +0200

    added test

--- Nexpo.Tests/Controllers/FAQControllerTest.cs
+++ Nexpo.Tests/Controllers/FAQControllerTest.cs
@@ -8,6 +8,12 @@ using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 using Xunit;
 using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Nexpo.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Nexpo.Models;
+using System.Collections.Generic;
 
 namespace Nexpo.Tests.Controllers
 {
@@ -15,10 +21,28 @@ namespace Nexpo.Tests.Controllers
     {
 
         [Fact]
+        
         public async Task GetAll()
         {
+            // Non logged in
+            var application = new WebApplicationFactory<Program>();
+            var client = application.CreateClient();
 
+            // Get response
+            var response = await client.GetAsync("/api/companies/");
+            Assert.True(response.StatusCode.Equals(HttpStatusCode.OK), "Wrong StatusCode. Expected: OK. Received: " + response.StatusCode.ToString());
+
+            // Sample result
+            var responseList = JsonConvert.DeserializeObject<List<FrequentAskedQuestion>>(await response.Content.ReadAsStringAsync());
+            
+            var numberOfQuestions = responseList.Count;
+            Assert.True(numberOfQuestions == numberOfQuestions + 1, "Wrong number of question. Expected: " + (numberOfQuestions + 1) + ". Received: " + numberOfQuestions);
+            
+            // Verify result
+            
         }
+            
+        
 
         [Fact]
         public async Task Get()
