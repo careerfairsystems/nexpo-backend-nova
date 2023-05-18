3abe9cd312ca532bf57445d702916e63ad31508b
Isa Randsalu <isarandsalu@Isas-MacBook-Air-8.local>
2023-05-18 18:24:47 +0200

    changes in tests

---
+++ Nexpo.Tests/Controllers/FAQControllerTest.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using Xunit;
+using System.IO;
+
+namespace Nexpo.Tests.Controllers
+{
+    public class FAQControllerTest
+    {
+
+        [Fact]
+        public async Task GetAllContacts()
+        {
+
+        }
+
+        [Fact]
+        public async Task GetContact()
+        {
+
+        }
+
+        [Fact]
+        public async Task UpdateContact()
+        {
+
+        }
+
+        [Fact]
+        public async Task CreateContact()
+        {
+
+        }
+
+        [Fact]
+        public async Task DeleteContact()
+        {
+
+        }
+
+    }
+}
