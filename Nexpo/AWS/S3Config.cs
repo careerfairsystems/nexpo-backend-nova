4eaaf918d923947bfdfb9226f13ef765ce5c182e
Alexander Hansson <nils.alexander.hansson@gmail.com>
2023-06-13 00:19:54 +0200

    Need to add secrets to config

--- Nexpo/AWS/S3Config.cs
+++ Nexpo/AWS/S3Config.cs
@@ -1,16 +1,27 @@
-﻿namespace Nexpo.AWS
+﻿using Microsoft.Extensions.Configuration;
+using System;
+namespace Nexpo.AWS
 {
     public class S3Config : IS3Configuration
     {
         // Keep the following details in appsettings.config file or DB or Enivironment variable
         // Get those values from it and assign to the below varibales. Based on the approach , modify the below code.
-        public S3Config()
+        public S3Config(IConfiguration configuration)
         {
             BucketName = "cvfiler";
             Region = "eu-north-1";
-            AwsAccessKey = "AKIAX3BYI22ZD733TJZ3";
-            AwsSecretAccessKey = "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";
+
+            // These are stored as user secrets in the project. 
+            // Should currently be available via bitwarden
+            Console.WriteLine("...");
+            Console.WriteLine("AWS Access Key: " + configuration["AwsAccessKey"]);
+            Console.WriteLine("AWS Secret Access Key: " + configuration["AwsSecretAccessKey"]);
+
+            AwsAccessKey = configuration["AWS:AwsAccessKey"];
+            AwsSecretAccessKey = configuration["AWS:AwsSecretAccessKey"];
+
         }
+
 
         public string BucketName { get; set; }
         public string Region { get; set; }
