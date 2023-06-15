0074cbe05c3f23dabdf16d8bf06b7186c82a11f6
Alexander Hansson <nils.alexander.hansson@gmail.com>
2023-06-15 16:52:16 +0200

    Refactoring

--- Nexpo/DTO/Events/CreateEventDTO.cs
+++ Nexpo/DTO/Events/CreateEventDTO.cs
@@ -5,7 +5,7 @@ namespace Nexpo.DTO
     /// <summary>
     /// DTO for adding an event to the database
     /// </summary>
-    public class AddEventDTO
+    public class CreateEventDTO
     {
         public string Name { get; set; }
 
