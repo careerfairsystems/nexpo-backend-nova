aeea29d358213dd41cc68280e5dd4f27ab73cc2c
Alexander Hansson <nils.alexander.hansson@gmail.com>
2023-09-05 23:53:20 +0200

    Signup company now works with json as input

--- UploadToDB/Company/signupCompany.py
+++ UploadToDB/Company/signupCompany.py
@@ -1,35 +1,33 @@
 import requests
 import json
-
 import sys
+
 sys.path.append("..")
 import login
 
-"""
-Create a company representative login, using JSON
-"""
+def read_json_file(filename):
+    with open(filename, 'r') as f:
+        return json.load(f)
 
-auth_token = login.get_token()
+def send_invitations(auth_token, representatives):
+    url = "https://www.nexpo.arkadtlth.se/api/signup/representative"
+    headers = {
+        'Authorization': auth_token,
+        'Content-Type': 'application/json'
+    }
+    
+    for rep in representatives:
+        response = requests.post(url, headers=headers, json=rep)
+        
+        if response.status_code == 204:
+            print(f"Successfully invited representative {rep['email']}.")
+        else:
+            print(f"Failed to invite representative {rep['email']}. Status code: {response.status_code}")
+            print(response.text)
 
-url = "https://www.nexpo.arkadtlth.se/api/signup/representative"
-
-headers = {
-    'Authorization': auth_token,
-    'Content-Type': 'application/json'
-}
-
-data_to_send = {
-    "email": "backend.arkad@box.tlth.se",
-    "firstname": "Backend",
-    "lastname": "Arkad",
-    "companyid": 180
-}
-
-response = requests.post(url, headers=headers, json=data_to_send)
-
-if response.status_code == 204:
-    print(f"Successfully invited representative.")
-else:
-    print(f"Failed to invite representative. Status code: {response.status_code}")
-    print(response.text)
-
+if __name__ == "__main__":
+    auth_token = login.get_token()
+    filename = "../jsonTemplate/signupRep.json"  # Or any filename you choose
+    
+    representatives = read_json_file(filename)
+    send_invitations(auth_token, representatives)
