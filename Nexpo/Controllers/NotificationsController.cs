e0b502f7ddf4879f89cf547d03023ce4a3db4189
Alexander Hansson <nils.alexander.hansson@gmail.com>
2023-09-10 19:40:01 +0200

    Errors but the general structure is down

--- Nexpo/Controllers/NotificationsController.cs
+++ Nexpo/Controllers/NotificationsController.cs
@@ -19,6 +19,44 @@ namespace Nexpo.Controllers
     [ApiController]
     public class NotificationController : ControllerBase
     {
+        public NotificationController()
+        {
+            FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromFile("path/to/your/firebase/credentials.json"),
+            });
+        }
+
+        [HttpPost("register")]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> RegisterUser(RegisterUserDTO dto)
+        {
+            if (string.IsNullOrEmpty(dto.Token))
+            {
+                return BadRequest("Token is required.");
+            }
+
+            // Initialize Firebase Admin SDK if not already initialized
+            var app = FirebaseApp.DefaultInstance ?? FirebaseApp.Create(new AppOptions
+            {
+                Credential = GoogleCredential.FromFile("path/to/your/firebase/credentials.json"),
+            });
+
+            string topic = dto.Topic;
+
+            // Register the provided token to the topic
+            var messaging = FirebaseMessaging.DefaultInstance;
+            var registrationTokens = new List<string>
+            {
+                dto.Token
+            };
+
+            TopicManagementResponse response = await messaging.SubscribeToTopicAsync(registrationTokens, topic);
+
+            return Ok();
+        }
+
         /// <summary>
         /// The api that the admin can use to send notifications to all users
         /// </summary>
@@ -27,23 +65,26 @@ namespace Nexpo.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> NotifyAll(NotificationDTO dto)
         {
-            // console log
+            if (string.IsNullOrEmpty(dto.Topic))
+            {
+                return BadRequest("Topic is required.");
+            }
 
-            if(String.IsNullOrEmpty("title") || String.IsNullOrEmpty("message"))
+            if (String.IsNullOrEmpty(dto.Message))
             {
                 return BadRequest();
             }
 
             var message = new Message()
             {
-                
+
                 Notification = new Notification
                 {
                     Title = dto.Title,
                     Body = dto.Message,
                 },
 
-                Topic = "all"
+                Topic = dto.Topic
             };
 
 
@@ -51,7 +92,7 @@ namespace Nexpo.Controllers
             var result = await messaging.SendAsync(message);
             Console.WriteLine(result); //projects/myapp/messages/2492588335721724324
 
-            
+
 
             return Ok();
 
