78c11e38628aa7eed4b9feda0c2c47daf5469050
Isa Randsalu <isarandsalu@Isas-MacBook-Air-8.local>
2023-09-21 17:30:01 +0200

    Adds script for deleting faq

---
+++ UploadToDB/FAQ/deleteFAQ.py
@@ -0,0 +1,31 @@
+import requests
+
+import sys
+sys.path.append("..")
+import login
+
+"""
+Delete a FAQ by using it id
+"""
+
+if len(sys.argv) > 1:
+    contact_id = sys.argv[1]
+else:
+    faq_id = input("Enter the faq id to delete: ")
+
+token = login.get_token()
+    
+url = f"https://www.nexpo.arkadtlth.se/api/faq/{faq_id}"
+
+headers = {
+    'Authorization': token,
+}
+
+r = requests.delete(url, headers=headers)
+
+print(r.status_code)
+
+if r.status_code == 200 or r.status_code == 204:
+    print(f"Successfully deleted faq with id {contact_id}.")
+else:
+    print(f"Failed to delete faq with id {contact_id}. Error: {r.text}")
