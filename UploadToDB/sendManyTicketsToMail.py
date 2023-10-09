c5dbb6fab65fa794aeb7129b32c4d998c8052504
Alexander Hansson <nils.alexander.hansson@gmail.com>
2023-10-09 18:58:52 +0200

    fix send many tickets

--- UploadToDB/sendManyTicketsToMail.py
+++ UploadToDB/sendManyTicketsToMail.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append("..")
 import login
 
-api_endpoint = "https://nexpo.arkadtlth.se/api/SendManyTicketsToMailAsync"
+api_endpoint = "https://nexpo.arkadtlth.se/api/Tickets/send"
 
 auth_token = login.get_token()
 
@@ -15,28 +15,33 @@ headers = {
     "Content-Type": "application/json"
 }
 
-def send_tickets_to_email(email, ticket_info):
+def send(mail, eventID, numberOfTickets):
     payload = {
-        "email": email,
-        "tickets": ticket_info
+        "mail": mail,
+        "eventid": eventID,
+        "numberOfTickets": numberOfTickets
+        
     }
-
+    
     response = requests.post(api_endpoint, json=payload, headers=headers)
-
+    
     if response.status_code == 200:
         print("Tickets sent successfully.")
     else:
         print(f"Failed to send tickets. Status code: {response.status_code}")
-        print(response.text)
+        print(response.content)
+
+
+    
 
 with open(jsonfile, encoding="utf-8") as d:
     data = json.load(d)
 
     for entry in data:
-        email = entry['mail']
-        ticket_info = {
-            "lunch_tickets_day1": entry['lunch_tickets_day1'],
-            "lunch_tickets_day2": entry['lunch_tickets_day2'],
-            "banquet_tickets": entry['banquet_tickets']
-        }
-        send_tickets_to_email(email, ticket_info)
+        info = entry['tickets']
+        
+        send(info['mail'], 31, info['lunch_tickets_day1'])
+        #send(info['mail'], 22, info['lunch_tickets_day2'])
+        #send(info['mail'], 22, info['banquet_tickets'])
+
+        
