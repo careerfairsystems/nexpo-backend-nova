5bb3e49d85afb32203ee10080eba51c96d4bffc9
Alexander Hansson <nils.alexander.hansson@gmail.com>
2023-10-20 03:21:21 +0200

    Back to user

--- Nexpo/Models/StudentSessionApplication.cs
+++ Nexpo/Models/StudentSessionApplication.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace Nexpo.Models
