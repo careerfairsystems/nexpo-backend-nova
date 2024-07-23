da9e670f8cfa29ace54db86f309fdadd020eac94
Leo Fjätström <leofjatstrom@gmail.com>
2024-07-23 20:12:47 +0200

    woops

--- Nexpo/Models/UserNotification.cs
+++ Nexpo/Models/UserNotification.cs
@@ -2,7 +2,7 @@ namespace Nexpo.Models
 {
     public class UserNotification
     {
-        public string UserId { get; set; }
+        public int UserId { get; set; }
         public User User { get; set; }
         public int NotificationId { get; set; }
         public Notification Notification { get; set; }
