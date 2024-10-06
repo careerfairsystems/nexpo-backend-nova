e70327526d128fd96ef37aa86cd34233d7c4edba
Leo Fjätström <leofjatstrom@gmail.com>
2024-10-06 12:46:45 +0200

    send notifications by http to expo by tokens

--- Nexpo/Controllers/NotificationsController.cs
+++ Nexpo/Controllers/NotificationsController.cs
@@ -1,14 +1,13 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Nexpo.DTO.Notifications;
 using Nexpo.Models;
 using Nexpo.Services;
 using Nexpo.Repositories;
+using Microsoft.AspNetCore.Http;
 
 namespace Nexpo.Controllers
 {
@@ -31,24 +30,24 @@ namespace Nexpo.Controllers
         /// Create a new notification
         /// </summary>
         [HttpPost]
-        //[Authorize(Roles = nameof(Role.Administrator))]
-        [ProducesResponseType(typeof(Notification), StatusCodes.Status200OK)]
-        public async Task<ActionResult> CreateNotification(AddNotificationDTO DTO)
+        [Route("create")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> CreateNotification(AddNotificationDTO dto)
         {
             var notification = new Notification
             {
-                Message = DTO.Message,
-                ScheduledTime = DTO.ScheduledTime,
-                NotificationType = DTO.NotificationType,
-                EventId = DTO.EventId
+                Message = dto.Message,
+                ScheduledTime = dto.ScheduledTime,
+                NotificationType = dto.NotificationType,
+                EventId = dto.EventId
             };
 
             await _notificationService.CreateNotificationAsync(notification);
 
-            // If the notification is has no scheduled time, send it now
+            // If the notification has no scheduled time, send it now
             if (notification.ScheduledTime == null)
             {
-                await SendNotifications(notification);
+                await SendNotifications(notification, dto.Title);
             }
 
             return Ok(notification);
@@ -59,9 +58,8 @@ namespace Nexpo.Controllers
         /// </summary>
         [HttpPost]
         [Route("send/{id}")]
-        //[Authorize(Roles = nameof(Role.Administrator))]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult> SendNotification(int id)
+        public async Task<ActionResult> SendNotification(int id, [FromBody] NotificationDTO dto)
         {
             var notification = await _notificationService.GetNotificationByIdAsync(id);
             if (notification == null)
@@ -69,91 +67,43 @@ namespace Nexpo.Controllers
                 return NotFound();
             }
 
-            await SendNotifications(notification);
+            await SendNotifications(notification, dto.Title);
             return Ok();
-        }
-
-        /// <summary>
-        /// Subscribe a user to event notifications
-        /// </summary>
-        [HttpPost]
-        [Route("subscribe")]
-        //[Authorize]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult> SubscribeToEvent(SubscribeNotificationDTO DTO)
-        {
-            await _notificationService.SubscribeUserToEventAsync(DTO.UserId, DTO.EventId);
-            return Ok();
-        }
-
-        /// <summary>
-        /// Unsubscribe a user from event notifications
-        /// </summary>
-        [HttpPost]
-        [Route("unsubscribe")]
-        //[Authorize]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult> UnsubscribeFromEvent(SubscribeNotificationDTO DTO)
-        {
-            await _notificationService.UnsubscribeUserFromEventAsync(DTO.UserId, DTO.EventId);
-            return Ok();
-        }
-
-        /// <summary>
-        /// Get notifications for a specific user
-        /// </summary>
-        [HttpGet]
-        [Route("user/{userId}")]
-        [ProducesResponseType(typeof(IEnumerable<NotificationDTO>), StatusCodes.Status200OK)]
-        public async Task<ActionResult> GetUserNotifications(int userId)
-        {
-            var notifications = await _notificationService.GetNotificationsForUserAsync(userId);
-            var notificationDTOs = notifications.Select(notification => new NotificationDTO
-            {
-                Id = notification.Id,
-                Message = notification.Message,
-                ScheduledTime = notification.ScheduledTime,
-                NotificationType = notification.NotificationType,
-                EventId = notification.EventId
-            });
-
-            return Ok(notificationDTOs);
         }
 
         /// <summary>
         /// Private method to send notifications based on NotificationType
         /// </summary>
-        private async Task SendNotifications(Notification notification)
+        private async Task SendNotifications(Notification notification, string title)
         {
+            string defaultTitle = string.IsNullOrEmpty(title) ? "Notification" : title;
+
             switch (notification.NotificationType)
             {
                 case NotificationType.General:
-                    // Send to all users
                     var allUsers = await _userRepo.GetAll();
                     foreach (var user in allUsers)
                     {
-                        await _notificationService.SendNotificationToUser(notification.Id, user.Id.Value);
+                        await _notificationService.SendNotificationToUser(notification.Id, user.Id.Value, defaultTitle);
                     }
                     break;
 
                 case NotificationType.EventReminder:
-                    // Send to users subscribed to the event
                     if (notification.EventId.HasValue)
                     {
                         var eventUsers = await _ticketRepo.GetAllUsersForEvent(notification.EventId.Value);
                         foreach (var user in eventUsers)
                         {
-                            await _notificationService.SendNotificationToUser(notification.Id, user.Id.Value);
+                            await _notificationService.SendNotificationToUser(notification.Id, user.Id.Value, defaultTitle);
                         }
                     }
                     break;
 
                 case NotificationType.RoleBased:
-                    // Send to users based on role
                     var roleUsers = await _userRepo.GetUsersByRole(Role.Volunteer);
                     foreach (var user in roleUsers)
                     {
-                        await _notificationService.SendNotificationToUser(notification.Id, user.Id.Value);
+                        await _notificationService.SendNotificationToUser(notification.Id, user.Id.Value, defaultTitle);
                     }
                     break;
 
--- Nexpo/DTO/Notifications/NotificationDTO.cs
+++ Nexpo/DTO/Notifications/NotificationDTO.cs
@@ -7,18 +7,7 @@ namespace Nexpo.DTO.Notifications
 {
     public class NotificationDTO
     {
-        public int Id { get; set; }
-        [Required]
+        public string Title { get; set; }
         public string Message { get; set; }
-
-        public DateTime? ScheduledTime { get; set; }
-
-        [Required]
-        public NotificationType NotificationType { get; set; }
-
-        public int? EventId { get; set; }
-
-        [Required]
-        public List<int> UserIds { get; set; }
     }
 }
